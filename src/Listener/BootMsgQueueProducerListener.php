5c9230435bd27de652acc53f20c96dd7bc40c4cd
李铭昕 <715557344@qq.com>
2023-05-22 20:17:14 +0800

    自动创建 AMQP 发布者

---
+++ src/Listener/BootMsgQueueProducerListener.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Octopus\Listener;
+
+use Hyperf\Amqp\Annotation\Producer as AmqpProducer;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Di\Annotation\AnnotationCollector;
+use Hyperf\Event\Annotation\Listener;
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Framework\Event\BootApplication;
+use Hyperf\Octopus\MsgQueue\Amqp\EventProducer;
+
+#[Listener]
+class BootMsgQueueProducerListener implements ListenerInterface
+{
+    public function __construct(protected ConfigInterface $config)
+    {
+    }
+
+    public function listen(): array
+    {
+        return [BootApplication::class];
+    }
+
+    public function process(object $event): void
+    {
+        $config = $this->config->get('octopus.mq');
+        $driver = $config['driver'];
+        switch ($driver) {
+            case 'amqp':
+                $options = array_merge(
+                    [
+                        'exchange' => 'octopus',
+                        'routing_key' => 'octopus.event',
+                        'queue_prefix' => 'octopus.event.queue.',
+                    ],
+                    $config['drivers']['amqp'] ?? []
+                );
+
+                AnnotationCollector::collectClass(EventProducer::class, AmqpProducer::class, new AmqpProducer($options['exchange'], $options['routing_key']));
+                break;
+        }
+    }
+}
