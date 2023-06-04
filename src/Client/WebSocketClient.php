1ed3f795bcde2083ff176c9fa1cbf62a0658ba20
李铭昕 <715557344@qq.com>
2023-06-04 14:19:40 +0800

    完善 ws client

--- src/Client/WebSocketClient.php
+++ src/Client/WebSocketClient.php
@@ -11,13 +11,27 @@ declare(strict_types=1);
  */
 namespace Hyperf\Octopus\Client;
 
+use Hyperf\Codec\Json;
 use Hyperf\Octopus\Client;
 use Hyperf\Octopus\Event\EventInterface;
+use Hyperf\WebSocketServer\Sender;
+use Psr\Container\ContainerInterface;
 
 class WebSocketClient extends Client
 {
+    protected Sender $sender;
+
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct($container);
+        $this->sender = $container->get(Sender::class);
+    }
+
     public function handle(EventInterface $event): bool
     {
+        foreach ($event->getFds() as $fd) {
+            $this->sender->push($fd, Json::encode($event->getPayload()));
+        }
         return true;
     }
 }
